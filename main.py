6fb02b7f4687d6c77fcbb6a361b4a3082017d86f
Reinhardt Erasmus <65610257+Spottie97@users.noreply.github.com>
2023-08-13 10:53:15 +0200

    Update main.py
    - Added Draw and Background functions
    - Draw adds the tiles to the window while background determines the image to draw and the area it needs to cover

--- main.py
+++ main.py
@@ -8,15 +8,35 @@ pygame.init()
 
 pygame.display.set_caption("Platfromer")
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+##Background: Determine the amount of tiles I will need and load the image
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+    return tiles, image
+
+##Draw Background Function
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
+
+##Main 
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Blue.png")
 
     run = True
     while run:
@@ -26,6 +46,7 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+        draw(window, background, bg_image)
     pygame.quit()
     quit()
 
