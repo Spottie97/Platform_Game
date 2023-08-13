a7b7bac7de45c5f73ecc2c10885322e54205d0b4
Reinhardt Erasmus <65610257+Spottie97@users.noreply.github.com>
2023-08-13 11:22:44 +0200

    Update main.py
    Added Gravity and Falling Acceleration effect to it

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 ##Character class
 class Player(pygame.sprite.Sprite):
     COLOR =(255, 0, 0)
+    GRAVITY = 1
+
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
         ## Velocity determines how fast player will move per frame
@@ -27,6 +29,7 @@ class Player(pygame.sprite.Sprite):
         self.mask = None
         self.direction = "left"
         self.animation_count = 0
+        self.fall_count = 0
 
     def move(self, dx, dy):
         self.rect.x += dx
@@ -45,7 +48,10 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def loop(self, fps):
+        self.y_vel += min(1,(self.fall_count / fps)* self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
+
+        self.fall_count += 1
 
     def draw(self, win):
         pygame.draw.rect(win, self.COLOR, self.rect)
@@ -72,6 +78,7 @@ def draw(window, background, bg_image, player):
 
     pygame.display.update()
 
+##Movement keys for the player
 def  handle_move(player):
     keys = pygame.key.get_pressed()
 
